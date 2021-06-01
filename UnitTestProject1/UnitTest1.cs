c058672ceaea79fe9ed4fea2f4452dfe8d416cb4
sohamdhamnaskar <sohamdham123@gmail.com>
2021-06-02 00:13:40 +0530

    [soham]Added:EnhancedInvoice

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace UnitTestProject1
         {
         }
         [Test]
-        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        public void GivenDistanceAndTime_ShouldReturn_TotalFare()
         {
             invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             double distance = 2.0;
@@ -25,13 +25,22 @@ namespace UnitTestProject1
             Assert.AreEqual(expected, fare);
         }
         [Test]
-        public void GivenMultipleRideShouldReturnInvoiceSummary()
+        public void GivenMultipleRide_ShouldReturn_InvoiceSummary()
         {
             invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 5) };
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
             InvoiceSummary expectedSummary = new InvoiceSummary(2, 35.0);
             Assert.AreEqual(expectedSummary.GetType(), summary.GetType());
+        }
+        [Test]
+        public void GivenMultipleRide_ShouldReturn_TotalNoRides_Fare_AverageFarePerRide()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 3) };
+            InvoiceSummary enhancedSummary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedEnhancedSummary = new InvoiceSummary(2, 30);
+            Assert.AreEqual(expectedEnhancedSummary, enhancedSummary);
         }
     }
 }
